272a2f0725f654cdf8f466eed26e9d697472cbbb
gaseema-n <gaseema@bitsoko.io>
2018-08-18 12:01:39 +0300

    login sign up view added & app.js user.js update

--- app.js
+++ app.js
@@ -93,6 +93,12 @@ function isLoggedIn(req, res, next) {
     res.redirect('/');
 }
 
+// process the signup form
+app.post('/signup', passport.authenticate('local-signup', {
+    successRedirect: '/profile', // redirect to the secure profile section
+    failureRedirect: '/signup', // redirect back to the signup page if there is an error
+    failureFlash: true // allow flash messages
+}));
 
 
 // catch 404 and forward to error handler
--- models/user.js
+++ models/user.js
@@ -1,10 +1,42 @@
 var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
 
-var UserSchema = new Schema({
-    username: String,
-    password: String,
-    email: String
+var userSchema = mongoose.Schema({
+
+    local: {
+        email: String,
+        password: String,
+    },
+    facebook: {
+        id: String,
+        token: String,
+        name: String,
+        email: String
+    },
+    twitter: {
+        id: String,
+        token: String,
+        displayName: String,
+        username: String
+    },
+    google: {
+        id: String,
+        token: String,
+        email: String,
+        name: String
+    }
+
 });
 
-var User = mongoose.model('User', UserSchema);
+// generating a hash
+userSchema.methods.generateHash = function (password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+// checking if password is valid
+userSchema.methods.validPassword = function (password) {
+    return bcrypt.compareSync(password, this.local.password);
+};
+
+// create the model for users and expose it to our app
+module.exports = mongoose.model('User', userSchema);
